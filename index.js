c1dedd0ebc880906c303e645749e4ca6f9ccd4e1
abdalla-wasaa <abdallawasaa7@gmail.com>
2023-07-13 12:07:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+ let multiply;
+ let num1 = 2;
+ let num2 = 31;
+
+ multiply = num1 * num2;
+ console.log(multiply);
+
+ let random;
+ random = Math.floor(Math.random() *10) + 1;
+ console.log(random);
+
+ let mod;
+ let num3 = 10;
+ let num4 = 6;
+ mod = num3%num4;
+ console.log(mod);
+
+ let max;
+ max = Math.max(2,3,10,5,16,18,20,6,12);
+ console.log(max);
